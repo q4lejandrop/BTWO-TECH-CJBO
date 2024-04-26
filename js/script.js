88048a5798edf1e11e9835c9a69fcc96ad49b418
tecnimux <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-04-26 17:00:20 -0500

    U

--- js/script.js
+++ js/script.js
@@ -2,27 +2,39 @@ window.addEventListener('scroll', function() {
 
     var div = document.querySelector('.box-01');
     var desplazamiento = window.scrollY;
-    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+    div.style.transform = 'translateY(-' + desplazamiento / 15 + 'px)';
 
 });
 window.addEventListener('scroll', function() {
 
     var div = document.querySelector('.box-02');
     var desplazamiento = window.scrollY;
-    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+    div.style.transform = 'translateY(-' + desplazamiento / 15 + 'px)';
 
 });
 window.addEventListener('scroll', function() {
 
     var div = document.querySelector('.box-03');
     var desplazamiento = window.scrollY;
-    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+    div.style.transform = 'translateY(-' + desplazamiento / 15 + 'px)';
 
 });
 window.addEventListener('scroll', function() {
 
     var div = document.querySelector('.box-04');
     var desplazamiento = window.scrollY;
-    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+    div.style.transform = 'translateY(-' + desplazamiento / 15 + 'px)';
 
 });
+
+window.addEventListener('scroll', () => {
+const scrollTop = document.documentElement.scrollTop;
+const scrollHeight = document.documentElement.scrollHeight;
+const clientHeight = document.documentElement.clientHeight;
+
+const scrolled = (scrollTop / (scrollHeight - clientHeight)) * 100;
+document.getElementById('statusBar').style.width = scrolled + '%';
+});
+
+
+

dc6f2df2776b38fffabca425df22adcd801b6476
tecnimux <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-04-26 00:45:11 -0500

    U

---
+++ js/script.js
@@ -0,0 +1,28 @@
+window.addEventListener('scroll', function() {
+
+    var div = document.querySelector('.box-01');
+    var desplazamiento = window.scrollY;
+    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+
+});
+window.addEventListener('scroll', function() {
+
+    var div = document.querySelector('.box-02');
+    var desplazamiento = window.scrollY;
+    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+
+});
+window.addEventListener('scroll', function() {
+
+    var div = document.querySelector('.box-03');
+    var desplazamiento = window.scrollY;
+    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+
+});
+window.addEventListener('scroll', function() {
+
+    var div = document.querySelector('.box-04');
+    var desplazamiento = window.scrollY;
+    div.style.transform = 'translateY(-' + desplazamiento / 5 + 'px)';
+
+});

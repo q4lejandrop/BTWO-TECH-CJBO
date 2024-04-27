92573af57c3e3b83e96aa7ac73090bf30568cd87
tecnimux <76859976+TECNIMUX-DEV@users.noreply.github.com>
2024-04-27 00:51:46 -0500

    UPDATE BRRRRRRRRUTALLLLLLLLLLLL

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Boxes
 window.addEventListener('scroll', function() {
 
     var div = document.querySelector('.box-01');
@@ -37,4 +38,32 @@ document.getElementById('statusBar').style.width = scrolled + '%';
 });
 
 
+// Change color logo
+window.addEventListener('scroll', () => {
+const logo = document.getElementById('logo');
+const distanceY = window.pageYOffset || document.documentElement.scrollTop;
+const scrollChangePoint = 75;
 
+if (distanceY > scrollChangePoint){
+logo.src = "/logo/main-logo-white-transparent-V02.png";
+}
+else {
+    logo.src = "/logo/logo-with-out-text.png";
+}
+
+});
+
+// Show div
+window.addEventListener('scroll', () => {
+    const borra = document.getElementById('borra');
+    const scrollThreashold = 100;
+    
+    if (window.scrollY > scrollThreashold){
+
+        borra.classList.add('borrador');
+    }
+
+    else {borra.classList.remove('borrador');
+
+    }
+});
